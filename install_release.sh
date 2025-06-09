675d61d713f4c6516bcecc72ba1bee0fd3044d2b
leonardorosa3660 <leonardorosa3660@gmail.com>
2025-06-09 19:50:25 -0300

    .

---
+++ install_release.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+BUILDDIR=build-auto
+PREBUILT_SERVER_URL=https://github.com/Genymobile/scrcpy/releases/download/v3.2/scrcpy-server-v3.2
+PREBUILT_SERVER_SHA256=b920e0ea01936bf2482f4ba2fa985c22c13c621999e3d33b45baa5acfc1ea3d0
+
+echo "[scrcpy] Downloading prebuilt server..."
+wget "$PREBUILT_SERVER_URL" -O scrcpy-server
+echo "[scrcpy] Verifying prebuilt server..."
+echo "$PREBUILT_SERVER_SHA256  scrcpy-server" | sha256sum --check
+
+echo "[scrcpy] Building client..."
+rm -rf "$BUILDDIR"
+meson setup "$BUILDDIR" --buildtype=release --strip -Db_lto=true \
+    -Dprebuilt_server=scrcpy-server
+cd "$BUILDDIR"
+ninja
+
+echo "[scrcpy] Installing (sudo)..."
+sudo ninja install
